dde45941488883c35a733c0c3bbdee6773cb3b7b
Olga Danylova <olga@themidnightcoders.com>
2018-06-19 14:27:45 +0300

    Initial Commit

---
+++ AppleTvDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppleTvDemo
+//
+//  Created by Olga Danylova on /19/618.
+//  Copyright © 2018 Olga Danylova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
